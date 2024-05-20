51f2ae7354983fecbf773920a5c4a8e0f0eed841
PedroAugusCoelho <peaugusto.coelho@gmail.com>
2024-05-20 20:58:44 -0300

    remove create user

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,3 @@ import { PrismaClient } from '@prisma/client'
 export const app = fastify()
 
 const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name: 'Pedro Augusto C. Costa',
-    email: 'pedro@gmail.com.br',
-  },
-})
